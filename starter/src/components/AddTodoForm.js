73c15d9d2381d3430cc833140bb2a359998e33dd
jzhengcode <jzhengcode@gmail.com>
2021-05-31 22:06:43 -0700

    toggle checks completed

--- starter/src/components/AddTodoForm.js
+++ starter/src/components/AddTodoForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addTodo } from '../redux/todoSlice';
 
 const AddTodoForm = () => {
--- starter/src/redux/store.js
+++ starter/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import todoReducer from './todoSlice'
+import todoReducer from './todoSlice';
+
 export default configureStore({
   reducer: {
     todos: todoReducer,
